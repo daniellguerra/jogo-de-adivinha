5cd8e9efa39829ec64091e87018e6fc5dfcfbc2b
Daniel Alcantara <dani@MacBook-Pro-de-Daniel.local>
2020-02-20 21:43:02 -0300

    #1: Tres tentativas implementada.

--- main.rb
+++ main.rb
@@ -1,7 +1,65 @@
-puts "Entre com um palpite:" 
-palpite = gets.chomp.to_i
+puts "Bem-vindo ao jogo da adivinhacao" 
+puts "Qual e o seu nome?"
+nome = gets
+puts "\n\n\n\n"
+puts "Comecaremos o jogo para voce, " + nome
+puts "Escolhendo um numero secreto entre 0 e 200..."
+numero_secreto = 121
+puts "Escolhido... que tal adivinhar hoje o nosso numero secreto?"
 
-numero_secreto = 69 
+puts "\n\n\n"
+puts "Tentativa 1"
+puts "Entre com o numero"
+chute = gets
+puts "Sera que acertou? Voce chutou " + chute
 
-puts "Voce acertou?"
-puts palpite == numero_secreto
+acertou = numero_secreto == chute.to_i
+
+if acertou
+    puts "Acertou!"
+else
+    maior = numero_secreto > chute.to_i
+    if maior
+        puts "O numero secreto e maior!"
+    else
+        puts "O numero secreto e menor!"
+    end 
+end
+
+puts "\n\n\n"
+puts "Tentativa 2"
+puts "Entre com o numero"
+chute = gets
+puts "Sera que acertou? Voce chutou " + chute
+
+acertou = numero_secreto == chute.to_i
+
+if acertou
+    puts "Acertou!"
+else
+    maior = numero_secreto > chute.to_i 
+    if maior
+        puts "O numero secreto e maior!"
+    else
+        puts "O numero secreto e menor!"
+    end
+end
+
+puts "\n\n\n"
+puts "Tentativa 3"
+puts "Entre com o numero"
+chute = gets
+puts "Sera que acertou? Voce chutou " + chute
+
+acertou = numero_secreto == chute.to_i
+
+if acertou
+    puts "Acertou!"
+else
+    maior = numero_secreto > chute.to_i
+    if maior
+        puts "O numero secreto e maior!"
+    else
+        puts "O numero secreto e menor!"
+    end 
+end
